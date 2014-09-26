f2e8c1c769ec122e5cf0729c93e02b714919897d
Rikard Andersson <mail@rikardandersson.se>
2014-09-26 08:12:29 +0200

    Removed unused imports

--- src/com/owera/xaps/web/app/util/WebProperties.java
+++ src/com/owera/xaps/web/app/util/WebProperties.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.owera.common.log.Logger;
