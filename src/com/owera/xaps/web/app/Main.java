be6e3e7a6e6a171a5e999fb0be78ced9288f65f1
morten <mortensimon@gmail.com>
2014-09-28 21:28:47 +0200

    v2.2.55 (2014-09-28) - add: Dashboard updated to add custom parameters - will make the dashboard much more useful. The parameters are configured in xaps-web.properties.

--- src/com/owera/xaps/web/app/Main.java
+++ src/com/owera/xaps/web/app/Main.java
@@ -39,7 +39,7 @@ public class Main extends HttpServlet {
 	private static Logger logger = new Logger();
 
 	/** The Constant version. */
-	public static final String version = "2.2.54";
+	public static final String version = "2.2.55";
 
 	/** The config. */
 	private Configuration config;
